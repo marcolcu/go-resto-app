8c34c48ee5a4b9b0192e74e42b49d96757d28224
PCGeorg <georgemarcellin00@gmail.com>
2024-09-23 14:19:34 +0700

    update railway

--- database/config.go
+++ database/config.go
@@ -4,7 +4,6 @@ import (
     "fmt"
     "os"
 
-    "github.com/joho/godotenv"
     "gorm.io/driver/mysql"
     "gorm.io/gorm"
 )
@@ -14,20 +13,14 @@ var DB *gorm.DB
 func ConnectDB() {
     var err error
 
-    // Load .env file
-    err = godotenv.Load()
-    if err != nil {
-        panic("Error loading .env file")
-    }
-
-    // Ambil konfigurasi dari environment variables
+    // Ambil konfigurasi dari environment variables yang disediakan oleh Railway
     user := os.Getenv("MYSQLUSER")
     password := os.Getenv("MYSQLPASSWORD")
     host := os.Getenv("MYSQLHOST")
     port := os.Getenv("MYSQLPORT")
     dbname := os.Getenv("MYSQLDATABASE")
 
-    // Format DSN menggunakan environment variables
+    // Format DSN menggunakan environment variables dari Railway
     DSN := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
         user, password, host, port, dbname)
 
