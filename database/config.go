2867752da1f7c336a40d929a912db65626a67366
PCGeorg <georgemarcellin00@gmail.com>
2024-09-23 08:30:34 +0700

    [UPDATE] Ubah jadi postgre

--- database/config.go
+++ database/config.go
@@ -2,8 +2,10 @@ package database
 
 import (
 	"fmt"
+	"os"
 
-	"gorm.io/driver/mysql"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -11,10 +13,27 @@ var DB *gorm.DB
 
 func ConnectDB() {
 	var err error
-	const MySQL = "indotim:indotim@tcp(127.0.0.1:3306)/resto-app-go?charset=utf8mb4&parseTime=True&loc=Local"
-	DSN := MySQL
 
-	DB, err = gorm.Open(mysql.Open(DSN), &gorm.Config{})
+	// Load .env file
+	err = godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	// Ambil konfigurasi dari environment variables
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+	sslmode := os.Getenv("DB_SSLMODE")
+	timezone := os.Getenv("DB_TIMEZONE")
+
+	// Format DSN menggunakan environment variables
+	DSN := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s",
+		host, user, password, dbname, port, sslmode, timezone)
+
+	DB, err = gorm.Open(postgres.Open(DSN), &gorm.Config{})
 	if err != nil {
 		panic("Can't connect to database")
 	}
